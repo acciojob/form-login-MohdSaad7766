32bfb45dea2b95ff0f357d5d4b511521cb72714a
Ansari-Mohd-Saad <157679873+MohdSaad7766@users.noreply.github.com>
2025-06-23 22:08:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+    let form = document.getElementById("form1");
+	let fname = form.element["fname"];
+	let lname = form.element["lname"];
+	
+	form.addEventListener('submit',()=>{
+		alert(`${fname} ${lname}`);
+	})
+
+	
 
 }
